43b43887f661735df10d31b7d2e6f75ce038b693
Jakyle <jjackson360@gmail.com>
2023-03-21 01:34:07 -0400

    lint

--- src/lib/Notifications.svelte
+++ src/lib/Notifications.svelte
@@ -1,29 +1,33 @@
 <script lang="ts">
-    import { fly, scale } from "svelte/transition";
-	import { classes } from "../util/html-helpers";
-	import { notificationStore, Notification, removeNotification, type NotificationType } from "./stores/notification-store";
+	import { fly, scale } from 'svelte/transition';
+	import { classes } from '../util/html-helpers';
+	import {
+		notificationStore,
+		Notification,
+		removeNotification,
+		type NotificationType
+	} from './stores/notification-store';
 
 	const alertMap: Record<NotificationType, string> = {
-		'info': 'alert-info',
-		'error': 'alert-error',
-		'success': 'alert-success',
-		'warning': 'alert-warning',
-	}
+		info: 'alert-info',
+		error: 'alert-error',
+		success: 'alert-success',
+		warning: 'alert-warning'
+	};
 
 	const notificationClasses = (notification: Notification) =>
-    classes('absolute', 'alert', 'pointer-events-auto', alertMap[notification]);
-
+		classes('absolute', 'alert', 'pointer-events-auto', alertMap[notification]);
 </script>
 
 {#await $notificationStore then notifications}
-  {#each notifications as { id, message, type } (id)}
-    <div
-      in:fly={{ y: -200, duration: 180 }}
-      out:scale={{ duration: 400 }}
-      class={notificationClasses(type)}
-    >
-      {message}
-      <button class="btn-sm btn-circle btn" on:click={() => removeNotification(id)}>X</button>
-    </div>
-  {/each}
+	{#each notifications as { id, message, type } (id)}
+		<div
+			in:fly={{ y: -200, duration: 180 }}
+			out:scale={{ duration: 400 }}
+			class={notificationClasses(type)}
+		>
+			{message}
+			<button class="btn-sm btn-circle btn" on:click={() => removeNotification(id)}>X</button>
+		</div>
+	{/each}
 {/await}
